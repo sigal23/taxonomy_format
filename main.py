e9a344413d796de05d52ef6c31587e9d43a2f874
sigal23 <sigal231097@gmail.com>
2022-07-04 17:05:49 +0300

    add gitignore
    Reference to the original files
    Correction of conditions and reference only to the concepts in concept_dict
    add tqdm

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
 import json
+from tqdm import tqdm
+
 
 if __name__ == '__main__':
 
     aliasesFields = {'synonym': '900000000000013009', 'fully specified name': '900000000000003001'}
 
-    with open('concepts.txt') as con:
+    with open('sct2_Concept_Snapshot_US1000124_20220301.txt') as con:
         concept_dict = {}
-        for line in con:
+        for line in tqdm(con):
             row = line.strip().split('\t')
             if row[2] == '1':
                 concept_dict[row[0]] = {'name': '', 'preferred synonym': [], 'synonyms': [],
                                         'preferred fully specified name': [], 'fully specified names': []}
 
-    with open('language.txt') as lang:
+    with open('der2_cRefset_LanguageSnapshot-en_US1000124_20220301.txt') as lang:
         prefer_terms = []
-        for line in lang:
+        for line in tqdm(lang):
             row = line.strip().split('\t')
             if row[2] == '1' and row[4] == '900000000000509007' and row[6] == '900000000000548007':
                 prefer_terms.append(row[5])
 
-    with open('description.txt') as desc:
-        for line in desc:
+    with open('sct2_Description_Snapshot-en_US1000124_20220301.txt') as desc:
+        for line in tqdm(desc):
             row = line.strip().split('\t')
-            if row[2] == '1' and row[5] == 'en':
-                if row[6] == aliasesFields['synonym'] and row[0] in prefer_terms:
-                    concept_dict[row[4]]['preferred synonym'].append(row[7])
-                    concept_dict[row[4]]['name'] = row[7]
-                elif row[6] == aliasesFields['fully specified name'] and row[0] in prefer_terms:
-                    concept_dict[row[4]]['preferred fully specified name'].append(row[7])
-                elif row[6] == aliasesFields['synonym']:
-                    concept_dict[row[4]]['synonyms'].append(row[7])
+            if row[2] == '1' and row[5] == 'en' and row[4] in concept_dict:
+                if row[6] == aliasesFields['synonym']:
+                    if row[0] in prefer_terms:
+                        concept_dict[row[4]]['preferred synonym'].append(row[7])
+                        concept_dict[row[4]]['name'] = row[7]
+                    else:
+                        concept_dict[row[4]]['synonyms'].append(row[7])
                 elif row[6] == aliasesFields['fully specified name']:
-                    concept_dict[row[4]]['fully specified names'].append(row[7])
+                    if row[0] in prefer_terms:
+                        concept_dict[row[4]]['preferred fully specified name'].append(row[7])
+                    else:
+                        concept_dict[row[4]]['fully specified names'].append(row[7])
 
-    with open('relationships.txt') as relation:
+    with open('sct2_Relationship_Snapshot_US1000124_20220301.txt') as relation:
         is_a_dict = {}
         concept_without_father = list(concept_dict.keys())
-        for line in relation:
+        for line in tqdm(relation):
             row = line.strip().split('\t')
-            if row[2] == '1' and row[7] == '116680003':
+            if row[2] == '1' and row[7] == '116680003' and row[4] in concept_dict and row[5] in concept_dict:
                 if row[5] in is_a_dict:
                     is_a_dict[row[5]].append(row[4])
                 else:
