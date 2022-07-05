84772aea69ab285d361a55759603f977cc9dcfa8
sigal23 <sigal231097@gmail.com>
2022-07-05 12:59:07 +0300

    Correction of code according to the review

--- main.py
+++ main.py
@@ -1,70 +1,7 @@
 import json
-from tqdm import tqdm
 
+from snomed.snomed_extract import snomed_extract
 
 if __name__ == '__main__':
 
-    aliasesFields = {'synonym': '900000000000013009', 'fully specified name': '900000000000003001'}
-
-    with open('sct2_Concept_Snapshot_US1000124_20220301.txt') as con:
-        concept_dict = {}
-        for line in tqdm(con):
-            row = line.strip().split('\t')
-            if row[2] == '1':
-                concept_dict[row[0]] = {'name': '', 'preferred synonym': [], 'synonyms': [],
-                                        'preferred fully specified name': [], 'fully specified names': []}
-
-    with open('der2_cRefset_LanguageSnapshot-en_US1000124_20220301.txt') as lang:
-        prefer_terms = []
-        for line in tqdm(lang):
-            row = line.strip().split('\t')
-            if row[2] == '1' and row[4] == '900000000000509007' and row[6] == '900000000000548007':
-                prefer_terms.append(row[5])
-
-    with open('sct2_Description_Snapshot-en_US1000124_20220301.txt') as desc:
-        for line in tqdm(desc):
-            row = line.strip().split('\t')
-            if row[2] == '1' and row[5] == 'en' and row[4] in concept_dict:
-                if row[6] == aliasesFields['synonym']:
-                    if row[0] in prefer_terms:
-                        concept_dict[row[4]]['preferred synonym'].append(row[7])
-                        concept_dict[row[4]]['name'] = row[7]
-                    else:
-                        concept_dict[row[4]]['synonyms'].append(row[7])
-                elif row[6] == aliasesFields['fully specified name']:
-                    if row[0] in prefer_terms:
-                        concept_dict[row[4]]['preferred fully specified name'].append(row[7])
-                    else:
-                        concept_dict[row[4]]['fully specified names'].append(row[7])
-
-    with open('sct2_Relationship_Snapshot_US1000124_20220301.txt') as relation:
-        is_a_dict = {}
-        concept_without_father = list(concept_dict.keys())
-        for line in tqdm(relation):
-            row = line.strip().split('\t')
-            if row[2] == '1' and row[7] == '116680003' and row[4] in concept_dict and row[5] in concept_dict:
-                if row[5] in is_a_dict:
-                    is_a_dict[row[5]].append(row[4])
-                else:
-                    is_a_dict[row[5]] = [row[4]]
-                if row[4] in concept_without_father:
-                    concept_without_father.remove(row[4])
-
-    # for k in concept_dict.keys():
-    #     print(str(k) + ' ' + str(concept_dict[k]))
-    # print('\n')
-    # for k in is_a_dict.keys():
-    #     print(str(k) + ' ' + str(is_a_dict[k]))
-    # print('\n')
-    # print(concept_without_father)
-
-    with open("concepts.json", 'w') as f:
-        json.dump(concept_dict, f)
-
-    with open("is_a.json", 'w') as f:
-        json.dump(is_a_dict, f)
-
-    with open("concept_without_father.json", 'w') as f:
-        json.dump(concept_without_father, f)
-
-    # loaded_json = json.load(f)
+    snomed_extract()
---
+++ snomed/snomed_extract.py
@@ -0,0 +1,82 @@
+import json
+from tqdm import tqdm
+import csv
+
+# Relevant IDs
+ACTIVE = '1'
+SYNONYM = '900000000000013009'
+FULLY_SPECIFIED_NAME = '900000000000003001'
+EN_USA = '900000000000509007'
+PREFERRED = '900000000000548007'
+IS_A = '116680003'
+# Relevant file names
+CONCEPT = 'snomed/sct2_Concept_Snapshot_US1000124_20220301.txt'
+LANGUAGE = 'snomed/der2_cRefset_LanguageSnapshot-en_US1000124_20220301.txt'
+DESCRIPTION = 'snomed/sct2_Description_Snapshot-en_US1000124_20220301.txt'
+RELATIONSHIP = 'snomed/sct2_Relationship_Snapshot_US1000124_20220301.txt'
+
+
+def snomed_extract():
+
+    # Save all active concepts in a dictionary called 'concept_dict',
+    # where the key is the ID of the concept and the value is a dictionary that contains for each concept:
+    # 'name', 'preferred synonym', 'synonyms', 'preferred fully specified name' and 'fully specified names'
+    with open(CONCEPT) as con:
+        concept_dict = {}
+        row_iter = csv.DictReader(con, delimiter='\t')
+        for row in tqdm(row_iter):
+            if row['active'] == ACTIVE:
+                concept_dict[row['id']] = {'name': '', 'preferred synonym': [], 'synonyms': [],
+                                           'preferred fully specified name': [], 'fully specified names': []}
+
+    # Save the ID of the preferred descriptions in an array called 'prefer_terms'
+    with open(LANGUAGE) as lang:
+        prefer_terms = []
+        row_iter = csv.DictReader(lang, delimiter='\t')
+        for row in tqdm(row_iter):
+            if row['active'] == ACTIVE and row['refsetId'] == EN_USA and row['acceptabilityId'] == PREFERRED:
+                prefer_terms.append(row['referencedComponentId'])
+
+    # Fill in the 'concept_dict' dictionary values with the appropriate descriptions for each concept
+    with open(DESCRIPTION) as desc:
+        row_iter = csv.DictReader(desc, delimiter='\t')
+        for row in tqdm(row_iter):
+            if row['active'] == ACTIVE and row['languageCode'] == 'en' and row['conceptId'] in concept_dict:
+                if row['typeId'] == SYNONYM:
+                    if row['id'] in prefer_terms:
+                        concept_dict[row['conceptId']]['preferred synonym'].append(row['term'])
+                        # The name of each concept will be the preferred synonym
+                        concept_dict[row['conceptId']]['name'] = row['term']
+                    else:
+                        concept_dict[row['conceptId']]['synonyms'].append(row['term'])
+                elif row['typeId'] == FULLY_SPECIFIED_NAME:
+                    if row['id'] in prefer_terms:
+                        concept_dict[row['conceptId']]['preferred fully specified name'].append(row['term'])
+                    else:
+                        concept_dict[row['conceptId']]['fully specified names'].append(row['term'])
+
+    # Create a dictionary called 'is_a_dict' that contains the relationship between the concepts according to their ID
+    # The key is the father and the value is an array of all his children
+    with open(RELATIONSHIP) as relation:
+        is_a_dict = {}
+        concept_without_father = list(concept_dict.keys())
+        row_iter = csv.DictReader(relation, delimiter='\t')
+        for row in tqdm(row_iter):
+            if row['active'] == ACTIVE and row['typeId'] == IS_A and row['sourceId'] in concept_dict and row['destinationId'] in concept_dict:
+                if row['destinationId'] in is_a_dict:
+                    is_a_dict[row['destinationId']].append(row['sourceId'])
+                else:
+                    is_a_dict[row['destinationId']] = [row['sourceId']]
+                # Saving the concepts that don't have a parent - will be used for building the hierarchy
+                if row['sourceId'] in concept_without_father:
+                    concept_without_father.remove(row['sourceId'])
+
+    # Saving the relevant data we extracted to json files
+    with open("snomed/concepts_test.json", 'w') as f:
+        json.dump(concept_dict, f)
+
+    with open("snomed/is_a_test.json", 'w') as f:
+        json.dump(is_a_dict, f)
+
+    with open("snomed/concept_without_father_test.json", 'w') as f:
+        json.dump(concept_without_father, f)
